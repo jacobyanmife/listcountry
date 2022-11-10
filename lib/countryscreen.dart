cb3ac4c22a1926892cdb842020f58c87a84bd811
Jacobyanmife <jacobyanmife@gmail.comm>
2022-11-10 22:51:37 +0100

    An app that display the list of countries

--- lib/countryscreen.dart
+++ lib/countryscreen.dart
@@ -11,6 +11,7 @@ class _CountryScreenState extends State<CountryScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
         leading: Image.asset('assets/logo1.png'),
         actions: [
@@ -23,17 +24,40 @@ class _CountryScreenState extends State<CountryScreen> {
               padding: EdgeInsets.all(10),
               child: Column(
                 children: [
-                  ListTile(
-                    leading: Icon(Icons.search),
-                    title: TextField(
-                      decoration: InputDecoration(
-                        labelText: 'Search Country',
+                  Container(
+                    height: 60,
+                    width: MediaQuery.of(context).size.width,
+                    color: Theme.of(context).primaryColor,
+                    child: Card(
+                      child: ListTile(
+                        leading: Icon(Icons.search),
+                        title: TextField(
+                          decoration: InputDecoration(
+                            labelText: 'Search Country',
+                            border: InputBorder.none,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                  ListView.builder( itemCount: 5, itemBuilder: (context, int index)  {
-                    return ListTile();
-                  })
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.language_outlined)),
+                      Spacer(),
+                      IconButton(
+                          onPressed: () {},
+                          icon: Icon(Icons.filter_alt_outlined)),
+                    ],
+                  ),
+                  // ListView.builder(
+                  //     itemCount: 5,
+                  //     itemBuilder: (context, int index) {
+                  //       return ListTile();
+                  //     })
                 ],
               ))),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,25 @@ import 'package:listcountry/countryscreen.dart';
 void main() {
   runApp(const MyApp());
 }
+final darkNotifier = ValueNotifier<bool>(false);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return ValueListenableBuilder <bool>(
+    valueListenable: darkNotifier, 
+    builder: (BuildContext context, bool isDark, Widget? child) {
+      return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(),
+      themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
+      theme: ThemeData(primaryColor: Colors.blue),
+      darkTheme: ThemeData.dark(),
       home: const CountryScreen(),
     );
+    });
+    
   }
 }
